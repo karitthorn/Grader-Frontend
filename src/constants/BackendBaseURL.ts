860bcdf58fb1b46d8256940372f11e520712e186
KanonKC <kanonkce@gmail.com>
2024-01-13 17:46:48 +0700

    Setup environment

--- src/constants/BackendBaseURL.ts
+++ src/constants/BackendBaseURL.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // export const BASE_URL = 'http://192.168.0.11:8000';
-export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = String(import.meta.env.VITE_BACKEND_URL);
 // axios.defaults.headers.common['account_id'] = localStorage.getItem('account_id') || null;
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token') || null;
